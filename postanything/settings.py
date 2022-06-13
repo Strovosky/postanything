9b476c2fb52a69a7a7d57ec63b7504004bd1552a
Strovosky <strovosky@gmail.com>
2022-06-13 17:37:31 -0500

    Set up django-on-heroku

--- postanything/settings.py
+++ postanything/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,5 @@ EMAIL_POST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("GMAIL")
 EMAIL_HOST_PASSWORD = os.getenv("GMAIL_PASSWORD")
+
+django_on_heroku.settings(locals())
