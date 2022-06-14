04abb82f049316dcc34e040368123d03dc2c72b8
Strovosky <strovosky@gmail.com>
2022-06-14 09:19:58 -0500

    Set debug to True

--- postanything/settings.py
+++ postanything/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG_VALUE")
-DEBUG = False 
+DEBUG = os.getenv("DEBUG_VALUE")
 
 ALLOWED_HOSTS = ['https://youpostanything.herokuapp.com/']
 
