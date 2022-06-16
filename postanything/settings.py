3e6378266f905a5c8201accc786e25c523602ac3
Strovosky <strovosky@gmail.com>
2022-06-16 16:30:24 -0500

    S3 Bucket Config

--- postanything/settings.py
+++ postanything/settings.py
@@ -155,12 +155,14 @@ django_on_heroku.settings(locals())
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# The name of the bucket
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 
 # This one specifies that in case an uploaded file has the name of another uploaded file
 # It won't overwrite it, but it will change its name.
 AWS_S3_FILE_OVERWRITE = False
-
 AWS_DEFAULT_ACL = None
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
