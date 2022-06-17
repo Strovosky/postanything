93269c59d4e4665e38d438ec84c80d271b598dd4
Strovosky <strovosky@gmail.com>
2022-06-17 16:08:18 -0500

    Added an app_name to the blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
+
 urlpatterns = [
     path("", view=views.home, name='blog_home'),
     path("topic/<int:pk>/<int:user_id>/", view=views.topic_detail_view, name="topic_detail"),
--- postanything/settings.py
+++ postanything/settings.py
@@ -162,7 +162,9 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 # It won't overwrite it, but it will change its name.
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
