b97e869f3cb6fc93f926ab1f0ba094399d37dacb
Strovosky <strovosky@gmail.com>
2022-06-17 16:26:55 -0500

    Added STATICFILES_STORAGE

--- postanything/settings.py
+++ postanything/settings.py
@@ -164,7 +164,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
